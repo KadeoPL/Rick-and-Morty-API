dc75b5292d5b88dcce6b1e0079b7a5036a2a6aef
KadeoPL <stopkakrzysztof1@gmail.com>
2024-03-08 22:22:28 +0100

    add style in style.css, create variables in js

---
+++ main.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+const charactersSection = document.querySelector('.characters-section');
+
+const characterName = document.querySelector('.character-name');
+const characterLocation = document.querySelector('.character-location');
+const characterStatus = document.querySelector('.character-status');
+const characterImage = document.querySelector('.character-box-image');
