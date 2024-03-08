0fc675fd8e6937d95c2a79a0721feda133beedff
KadeoPL <stopkakrzysztof1@gmail.com>
2024-03-08 22:35:04 +0100

    add getCharacters function

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 
@@ -10,3 +9,18 @@ const characterName = document.querySelector('.character-name');
 const characterLocation = document.querySelector('.character-location');
 const characterStatus = document.querySelector('.character-status');
 const characterImage = document.querySelector('.character-box-image');
+
+async function getCharacters(firstCharacterId, lastCharacterId) {
+    try {
+        for(let i = firstCharacterId; i <= lastCharacterId; i++){
+            const response = await axios.get(`https://rickandmortyapi.com/api/character/${i}`);
+            console.log(response.data);
+        }
+    } catch (error) {
+        console.log(error.response.status);
+        console.log(error.response.data.detail);
+        throw error;
+    }
+}
+
+getCharacters(1, 5);
